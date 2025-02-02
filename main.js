7e15e39f729c97379ba8033aa62259e3d5b7ec48
Anuja Kumari <166990955+k-anuja06@users.noreply.github.com>
2025-02-02 15:48:47 +0530

    Changes in javascript

--- main.js
+++ main.js
@@ -67,16 +67,16 @@ window.addEventListener('scroll', scrollActive)
 
 
 /*=============== LOCK RIGHT CLICK - PREVENT INSPECT =============*/
-// document.addEventListener('contextmenu', function(e) {
-//     e.preventDefault();
-//   });
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
 
 
-//   document.addEventListener('keydown', function(e) {
-//     if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'J') || (e.ctrlKey && e.key === 'U')) {
-//       e.preventDefault();
-//     }
-//   });
+  document.addEventListener('keydown', function(e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'J') || (e.ctrlKey && e.key === 'U')) {
+      e.preventDefault();
+    }
+  });
 
 
 
